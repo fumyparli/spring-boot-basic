95bffda5f1db40b93a393448cd0bda5d009e3808
fumyparli <qkskskn@naver.com>
2021-04-13 01:52:37 +0900

    Add test code

--- src/main/java/ga/fumyparli/e2/OrderApp.java
+++ src/main/java/ga/fumyparli/e2/OrderApp.java
@@ -10,16 +10,16 @@ import ga.fumyparli.e2.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService=new MemberServiceImpl();
-        OrderService orderService=new OrderServiceImpl();
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
 
-        Long memberId=1L;
-        Member member= new Member(memberId,"member1", Grade.VIP);
+        Long memberId = 1L;
+        Member member = new Member(memberId, "member1", Grade.VIP);
         memberService.join(member);
 
-        Order order=orderService.createOrder(memberId,"item1",10000);
+        Order order = orderService.createOrder(memberId, "item1", 10000);
 
-        System.out.println("order: "+order.toString()); // println에 객체를 넣으면 객체의 toString이 불림
-        System.out.println("orderPrice: "+order.calculatePrice());
+        System.out.println("order: " + order.toString()); // println에 객체를 넣으면 객체의 toString이 불림
+        System.out.println("orderPrice: " + order.calculatePrice());
     }
 }
---
+++ src/test/java/ga/fumyparli/e2/order/OrderServiceTest.java
@@ -0,0 +1,23 @@
+package ga.fumyparli.e2.order;
+
+import ga.fumyparli.e2.member.Grade;
+import ga.fumyparli.e2.member.Member;
+import ga.fumyparli.e2.member.MemberService;
+import ga.fumyparli.e2.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "member1", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "item1", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
