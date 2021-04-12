b802c459b4f2c37832da3c0ed27fe0db732fd153
fumyparli <qkskskn@naver.com>
2021-04-13 02:06:05 +0900

    Complete test for rateDiscountPolicy

---
+++ src/main/java/ga/fumyparli/e2/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package ga.fumyparli.e2.discount;
+
+import ga.fumyparli.e2.member.Grade;
+import ga.fumyparli.e2.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ src/test/java/ga/fumyparli/e2/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,32 @@
+package ga.fumyparli.e2.discount;
+
+import ga.fumyparli.e2.member.Grade;
+import ga.fumyparli.e2.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 되어야 함")
+    void forVip() {
+        Member member = new Member(1L, "member1", Grade.VIP);
+
+        int discount = discountPolicy.discount(member, 12000);
+
+        Assertions.assertThat(discount).isEqualTo(1200);
+    }
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 되어야 함")
+    void forBasic() {
+        Member member = new Member(1L, "member1", Grade.BASIC);
+
+        int discount = discountPolicy.discount(member, 12000);
+
+        Assertions.assertThat(discount).isEqualTo(1200);
+    }
+}
