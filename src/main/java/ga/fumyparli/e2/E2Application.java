39406c043b840a7ec4263c25cb0317cef9a3551d
fumyparli <qkskskn@naver.com>
2021-04-13 00:21:30 +0900

    Init project using spring-boot

---
+++ src/main/java/ga/fumyparli/e2/E2Application.java
@@ -0,0 +1,13 @@
+package ga.fumyparli.e2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E2Application.class, args);
+	}
+
+}
