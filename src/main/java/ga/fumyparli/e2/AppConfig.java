78b31a79966e0c8ba2ed4c0c067f5bc39c6b84e9
fumyparli <qkskskn@naver.com>
2021-04-13 21:40:03 +0900

    Add DI

--- src/main/java/ga/fumyparli/e2/AppConfig.java
+++ src/main/java/ga/fumyparli/e2/AppConfig.java
@@ -1,6 +1,8 @@
 package ga.fumyparli.e2;
 
+import ga.fumyparli.e2.discount.DiscountPolicy;
 import ga.fumyparli.e2.discount.FixDiscountPolicy;
+import ga.fumyparli.e2.member.MemberRepository;
 import ga.fumyparli.e2.member.MemberService;
 import ga.fumyparli.e2.member.MemberServiceImpl;
 import ga.fumyparli.e2.member.MemoryMemberRepository;
@@ -9,12 +11,19 @@ import ga.fumyparli.e2.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 }
--- src/main/java/ga/fumyparli/e2/OrderApp.java
+++ src/main/java/ga/fumyparli/e2/OrderApp.java
@@ -16,7 +16,7 @@ public class OrderApp {
         Member member = new Member(memberId, "member1", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "item1", 10000);
+        Order order = orderService.createOrder(memberId, "item1", 12000);
 
         System.out.println("order: " + order.toString()); // println에 객체를 넣으면 객체의 toString이 불림
         System.out.println("orderPrice: " + order.calculatePrice());
